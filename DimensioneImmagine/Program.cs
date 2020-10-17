39e51669f56975f2ea819b51f89913aa76559bc2
miglio000 <gabrielemiglio23@gmail.com>
2020-10-17 11:06:30 +0200

    codice dimensione immagine

--- DimensioneImmagine/Program.cs
+++ DimensioneImmagine/Program.cs
@@ -6,10 +6,11 @@ namespace DimensioneImmagine
     {
         static void Main(string[] args)
         {
-            int colorazione, larghezza, altezza, pixel, b, kb;
-            Console.WriteLine("Choose the coloring of your image");
-            colorazione = int.Parse(Console.ReadLine());
+            int colorazione, larghezza, altezza, pixel, b, kb, temp;
 
+            Console.WriteLine("Choose the coloring of your image(2,4,8,16 bit)");
+            colorazione = int.Parse(Console.ReadLine());
+            temp = colorazione;
             Console.Write("Choose the width of your image: ");
             larghezza = int.Parse(Console.ReadLine());
             if (larghezza > 0)
@@ -18,11 +19,41 @@ namespace DimensioneImmagine
                 altezza = int.Parse(Console.ReadLine());
                 if (altezza > 0)
                 {
-                    pixel = larghezza * altezza;
-                    b=
+                    switch (colorazione)
+                    {
+                        case 2:
+                            pixel = larghezza * altezza;
+                            b = 2 / 8;
+                            kb = (pixel * b) / 1024;
+                            break;
+                        case 4:
+                            pixel = larghezza * altezza;
+                            b = 4 / 8;
+                            kb = (pixel * b) / 1024;
+                            break;
+                        case 8:
+                            pixel = larghezza * altezza;
+                            b = 8 / 8;
+                            kb = (pixel * b) / 1024;
+                            break;
+                        case 16:
+                            pixel = larghezza * altezza;
+                            b = 16 / 8;
+                            kb = (pixel * b) / 1024;
+                            break;      
+                    }
+                    Console.WriteLine($"The pixel of your image are: {pixel}, your image has {kb} kilobyte.");
+                }
+                else
+                {
+                    Console.WriteLine("Height not valid.");
                 }
             }
-
+            else
+            {
+                Console.WriteLine("Width not valid.");
+            }
+            
         }
     }
 }
