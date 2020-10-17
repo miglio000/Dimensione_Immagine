0f60cc23bf68fc562c4965f050448be95301d1d0
miglio000 <gabrielemiglio23@gmail.com>
2020-10-17 14:00:09 +0200

    dimensione immagine

--- DimensioneImmagine/Program.cs
+++ DimensioneImmagine/Program.cs
@@ -6,12 +6,12 @@ namespace DimensioneImmagine
     {
         static void Main(string[] args)
         {
-            double colorazione, larghezza, altezza, pixel, b, kb, temp;
+            double colorazione, larghezza, altezza, pixel, b, kb;
             pixel = 0;
             kb = 0;
-            Console.WriteLine("Choose the coloring of your image(2,4,8,16 bit)");
+            Console.WriteLine("Choose the coloring of your image between 1-24 bit:");
             colorazione = double.Parse(Console.ReadLine());
-            temp = colorazione;
+            
             Console.Write("Choose the width of your image: ");
             larghezza = double.Parse(Console.ReadLine());
             if (larghezza > 0)
@@ -20,29 +20,9 @@ namespace DimensioneImmagine
                 altezza = int.Parse(Console.ReadLine());
                 if (altezza > 0)
                 {
-                    switch (colorazione)
-                    {
-                        case 2:
-                            pixel = larghezza * altezza;
-                            b = 2 / 8;
-                            kb = (pixel * b) / 1024;
-                            break;
-                        case 4:
-                            pixel = larghezza * altezza;
-                            b = 4 / 8;
-                            kb = (pixel * b) / 1024;
-                            break;
-                        case 8:
-                            pixel = larghezza * altezza;
-                            b = 8 / 8;
-                            kb = (pixel * b) / 1024;
-                            break;
-                        case 16:
-                            pixel = larghezza * altezza;
-                            b = 16 / 8;
-                            kb = (pixel * b) / 1024;
-                            break;
-                    }
+                    pixel = larghezza * altezza;
+                    b = colorazione / 8;
+                    kb = (pixel * b) / 1024;
                     Console.WriteLine($"The pixel of your image are: {pixel}, your image has {kb} kilobyte.");
                 }
                 else
